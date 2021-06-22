da548caaa9fcd623fdf5b04c1842ef98684b1741
amcinvaille <alexa.mcinvaille@gmail.com>
2021-06-21 21:19:54 -0500

    load data

--- src/model/PlayerList.java
+++ src/model/PlayerList.java
@@ -8,7 +8,8 @@ public class PlayerList {
     private ArrayList<Player> playerArr;
     
     public PlayerList() {
-        
+        playerArr = new ArrayList<>();
+        loadArr();
     }
     
     public PlayerList(ArrayList playerArr) {
@@ -18,7 +19,22 @@ public class PlayerList {
     
     
     public void loadArr() {
-        //Is this the same as setPlayerArr?
+        Player p1 = new Player("Alexa", "password1234", "alexa_m", "67226");
+        Player p2 = new Player("Tomi", "password1234", "tomi_a", "28451");
+        Player p3 = new Player("Vincent", "password1234", "vincent_t", "13143");
+        Manager m1 = new Manager("Yasic", "password1234", "yasic_n", "39507", (new Store("Card Store", "900 Sandpiper Ln", "39507", 40)));
+        Manager m2 = new Manager("Will", "password1234", "will_m", "13143", (new Store("Game Store", "187 Yellow Ave", "13143", 50)));
+        Manager m3 = new Manager("Peggy", "password1234", "peggy_f", "67226", (new Store("Chess Store", "123 Bluebird St", "67226", 30)));
+        
+        playerArr.add(p1);
+        playerArr.add(p2);
+        playerArr.add(p3);
+        playerArr.add(m1);
+        playerArr.add(m2);
+        playerArr.add(m3);     
+        
+        
+        
     }
     
     public void addPlayer(Player player) {
--- src/tournamentapp/TournamentApp.java
+++ src/tournamentapp/TournamentApp.java
@@ -1,12 +1,19 @@
 
 package tournamentapp;
 
+import model.PlayerList;
+import model.StoreList;
+
 
 public class TournamentApp {
 
 
     public static void main(String[] args) {
-        
+        PlayerList pl = new PlayerList();
+        //StoreList storeList = new StoreList();
+        System.out.println("Player List: " + pl.getPlayerArr());
+        //System.out.println("StoreList: " + pl.getPlayerArr().get(4).);
+
     }
     
 }
